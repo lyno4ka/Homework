d8e10df88dc3ceaa7caa1109d9d3399f808b6f4c
lyno4ka <yuliyamasterenko@gmail.com>
2020-02-05 18:27:32 +0200

    Fixed homework

---
+++ 27.01.2020/script/script.js
@@ -0,0 +1,68 @@
+// Task1
+let a = 5;
+let b = a * 3;
+let c = a + b;
+console.log(a, b, c);
+
+
+// Task2
+// let firstName = prompt('Enter firstname: ', 'John').toUpperCase();
+// let lastName =  prompt('Enter lastname: ', 'Doe').toUpperCase();
+
+// alert(`What's up ${firstName} ${lastName}`);
+
+// Task3
+// let x = +prompt('Enter number', '2');
+// let y = +prompt('Enter number', '3');
+
+// alert(`Product of numbers: ${x*y}`);
+// alert(`Quotient of numbers: ${x/y}`);
+// alert(`Difference of numbers: ${x-y}`);
+// alert(`Sum of numbers: ${x+y}`);
+
+// Task4
+// let distance = 384400;
+// let speed = +prompt('Enter speed', '320');
+// let time = distance/speed;
+// alert(`Flight time: ${time} hours`);
+
+// Task5
+// let number = +prompt('Enter number: ', '4');
+// alert(`Odd number: ${!!(number % 2)}`); 
+
+// Task6
+// let value = prompt('Is it a number?', '1');
+// let result = Number(value);
+// alert(`Value is a number: ${(!!result)}`);
+
+// Task7
+// const unit = 1024;
+// let bytes = prompt('Enter number', '');
+// let kilobytes = (bytes / unit).toFixed(2);
+// let megabytes = (kilobytes / unit).toFixed(2);
+// let gigabytes = (megabytes / unit);
+
+// alert(`${kilobytes} kb`);
+// alert(`${megabytes} mb`);
+// alert(`${gigabytes} gb`);
+
+// Task8
+// let x = prompt('У Чжуан-цзы яблок', '10');
+// let y = prompt('Цзэн-Цзы съел яблок', '8');
+
+// alert(`Цзэн-Цзы говорит ${!!(x % y)}`);
+
+// Task9
+// let column1 = prompt('Enter height:', '50px');
+// let column2 = prompt('Enter height:', '20px');
+// let column3 = prompt('Enter height:', '30px');
+
+// let maxValue = Math.max(parseInt(column1), parseInt(column2), parseInt(column3));
+
+// alert(`Max value: ${maxValue}px`);
+
+// Task10
+// let randomNumber = Math.floor(Math.random() * 100);
+// let secondNumber = +prompt('Enter number:', '15');
+
+// alert(`randomNumber < secondNumber = ${!!(randomNumber < secondNumber)},  randomNumber = ${randomNumber}, secondNumber = ${secondNumber}`);
